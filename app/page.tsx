65fb79ef85617a57f8fc0365be4275df44d26dff
Akito UEHARA <akt.uehr@gmail.com>
2023-02-23 17:14:41 +0900

    feat: Create sample scene

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,48 @@
+"use client"
+
 import styles from './page.module.css'
 
+import { BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import WebGL from 'three/examples/jsm/capabilities/WebGL.js';
+
+import { useEffect } from 'react';
+
 export default function Home() {
+  useEffect(() => {
+    const scene = new Scene();
+    const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const renderer = new WebGLRenderer();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    const geometry = new BoxGeometry( 1, 1, 1 );
+    const material = new MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+
+    function animate() {
+      requestAnimationFrame( animate );
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render( scene, camera );
+    }
+
+    if ( WebGL.isWebGLAvailable() ) {
+      // Initiate function or other initializations here
+      animate();
+    } else {
+      const warning = WebGL.getWebGLErrorMessage();
+      document.body.appendChild( warning );
+    }
+    
+  }, [])
+
   return (
-    <main>
+    <main id="container">
       Hello three.js!
     </main>
   )
